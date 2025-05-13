46f56f4f211ec2bdb9c9384f43e32fe21c51bed3
Chinmay_Bhintade <chinmaybhintade2004@gmail.com>
2025-05-13 11:38:05 +0530

    Add files via upload

---
+++ Section_6/printing exercise.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//1*********
+//12********
+//123*******
+//1234******
+//12345*****
+
+int main()
+{
+	//here it can be done by using single printf fuunction rather than using many functions
+	printf("1*********\n12********\n123*******\n1234******\n12345*****\n");
+	
+	return 0;
+}
---
+++ Section_6/program of iscoceles triangle.cpp
@@ -0,0 +1,12 @@
+//c programming 
+//printing an isosceles triangle with asterick signs
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("*\n");
+	printf("***\n");
+	printf("*****\n");
+	
+	return 0;
+}
